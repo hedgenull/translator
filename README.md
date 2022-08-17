401256f4b54bac820175356b2aead1f3d1ccfe3e
Hedge Fleming <hedgefleming@gmail.com>
2022-08-16 22:13:10 -0400

    Added README, banner, etc.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Translator: A New Kind of Obfuscation
+
+Translator is a simple Python script that takes your input text, runs it through 10 (by default) different languages with Google Translate, and then spits out the final result converted back into English. It's a mostly useless, yet amusing, program. Here's a screenshot:
--- translator.py
+++ translator.py
@@ -1,10 +1,19 @@
+# Imports
 import random
 
+import art
 from colorama import Fore, init
 from deep_translator import GoogleTranslator
 
+# Initialize colorama, print banner
 init()
+print(Fore.LIGHTBLUE_EX)
+art.tprint("TRANSLATOR")
+print(f"{Fore.LIGHTYELLOW_EX}----- A New Kind of Obfuscation -----")
+print("----- By Hedge Fleming <hedgefleming@gmail.com> -----")
+print("----- Copyright (C) 2022. All Rights Reserved. -----")
 
+# Dictionary of language codes and full names
 LANGUAGES = {
     abbrev: fullname
     for fullname, abbrev in GoogleTranslator().get_supported_languages(True).items()
@@ -17,8 +26,9 @@ ENGLISH = "en"
 # Pick some random languages
 selected_langs = [random.choice(list(LANGUAGES.keys())) for _ in range(NUM_LANGS)]
 
+# Get the user's input
 text = input(
-    f"{Fore.LIGHTGREEN_EX}[*] Enter your phrase (multiple lines, press Control+C to finish):{Fore.LIGHTRED_EX}\n"
+    f"{Fore.LIGHTGREEN_EX}[*] Enter your text (multiple lines, press Control+C to finish):{Fore.LIGHTRED_EX}\n"
 )
 
 while True:
@@ -45,8 +55,11 @@ text = GoogleTranslator(source=last, target=ENGLISH).translate(text)
 print("[*] All done!")
 
 print(Fore.LIGHTYELLOW_EX + ("=" * 50))
-print(f"Original text:\n{Fore.LIGHTRED_EX}{original_text}{Fore.LIGHTYELLOW_EX}")
-print(f"Translated text:\n{Fore.LIGHTRED_EX}{text}{Fore.LIGHTYELLOW_EX}")
+print(f"Original text:\n{Fore.LIGHTRED_EX}")
+print(original_text + Fore.LIGHTYELLOW_EX)
+print(f"Translated text:\n{Fore.LIGHTRED_EX}")
+print(text + Fore.LIGHTYELLOW_EX)
+
 print(("=" * 50) + "\nLanguages used, in order of translation:" + Fore.LIGHTRED_EX)
 
 print("\n".join([LANGUAGES[abbrev].title() for abbrev in selected_langs]))
